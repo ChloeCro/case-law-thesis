6ce14acf6421e398d8472c086fa85cfa873f4edd
ChloeCro <chloe_crombach@hotmail.com>
2024-09-01 00:24:53 +0200

    added req for xlsx file, updated util function to load xlsx file

--- utils/util_functions.py
+++ utils/util_functions.py
@@ -1,3 +1,4 @@
+import os
 import ast
 import pandas as pd
 
@@ -15,12 +16,20 @@ def safe_literal_eval(val: str) -> any:
         return val
 
 
-def load_csv_to_df(file_path: str) -> pd.DataFrame:
+def load_file_to_df(file_path: str) -> pd.DataFrame:
     """
-    Load a CSV file into a Pandas DataFrame with safe literal evaluation to ensure correct datatypes.
-    :param file_path: The path to the file to laod.
-    :return: A Pandas DataFrame with correct datatypes
+    Load a CSV or XLSX file into a Pandas DataFrame with safe literal evaluation to ensure correct datatypes.
+    :param file_path: The path to the file to load.
+    :return: A Pandas DataFrame with correct datatypes.
     """
-    df = pd.read_csv(file_path)
+    file_extension = os.path.splitext(file_path)[1].lower()
+
+    if file_extension == '.csv':
+        df = pd.read_csv(file_path)
+    elif file_extension == '.xlsx':
+        df = pd.read_excel(file_path)
+    else:
+        raise ValueError(f"Unsupported file extension: {file_extension}")
+
     df = df.applymap(safe_literal_eval)
     return df
