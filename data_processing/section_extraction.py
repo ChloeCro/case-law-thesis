7e8eadc3c1dffa0cc01b77283126c59c84a0290a
ChloeCro <chloe_crombach@hotmail.com>
2024-09-18 09:42:01 +0200

    initial section_extraction script

---
+++ data_processing/section_extraction.py
@@ -0,0 +1,152 @@
+import multiprocessing
+import pandas as pd
+import os
+import re
+
+from collections import defaultdict
+from bs4 import BeautifulSoup
+
+from utils import constants, logger_script, util_data_loader
+
+logger = logger_script.get_logger(constants.EXTRACTION_LOGGER_NAME)
+
+class SectionExtractor:
+
+    # Function to concatenate the matched groups (excluding the space)
+    def replacer(self, match):
+        return f"artikel{match.group(1)}"
+
+    # Your text_sectioning function
+    def text_sectioning(self, doc, split_patterns, merge_patterns):
+        merge_patterns = merge_patterns[0]
+        text = re.sub(merge_patterns, self.replacer, doc)
+        super_pattern = "|".join(split_patterns)
+        sectioning = re.split(super_pattern, text)
+        stripped_list = [s.lstrip() for s in sectioning]
+        filtered_list = [s for s in stripped_list if re.match(r'\d+\.', s)]
+        return filtered_list
+
+    # Function to extract text without XML tags
+    def extract_text_from_section(self, section):
+        # Join all content within the section as a single string
+        section_text = ''.join(str(child) for child in section.contents)
+        # Create a new BeautifulSoup object to parse the content and strip tags
+        clean_text = BeautifulSoup(section_text, 'html.parser').get_text()
+        return clean_text
+
+    def process_xml(self, xml_file):
+        with open(xml_file, 'r', encoding='utf-8') as file:
+            soup = BeautifulSoup(file, 'xml')
+
+            # Initialize variables
+            procesverloop_text, overwegingen_text, beslissing_text = '', '', ''
+            ecli, date, inhoud, legal_body, rechtsgebied, wetsverwijzing = '', '', '', '', '', ''
+
+            # Extract global information
+            ecli_tag = soup.find("dcterms:identifier")
+            date_tag = soup.find("dcterms:date", {"rdfs:label": "Uitspraakdatum"})
+            inhoud_tag = soup.find("inhoudsindicatie")
+            legal_body_tag = soup.find("dcterms:creator", {"rdfs:label": "Instantie"})
+            rechtsgebied_tag = soup.find("dcterms:subject", {"rdfs:label": "Rechtsgebied"})
+            wetsverwijzing_tag = soup.find("dcterms:references", {"rdfs:label": "Wetsverwijzing"})
+
+            if ecli_tag: ecli = ecli_tag.text
+            if date_tag: date = date_tag.text
+            if inhoud_tag: inhoud = inhoud_tag.text
+            if legal_body_tag: legal_body = legal_body_tag.text
+            if rechtsgebied_tag: rechtsgebied = rechtsgebied_tag.text
+            if wetsverwijzing_tag: wetsverwijzing = wetsverwijzing_tag.text
+
+
+            # Process each section and convert to pure text
+            sections = soup.find_all("section")
+            for sec in sections:
+                role = sec.get('role')
+
+                # Use a common method to extract plain text
+                section_text = self.extract_text_from_section(sec)
+
+                # Append text based on the role of the section
+                if role == 'procesverloop':
+                    procesverloop_text += (' ' + section_text if procesverloop_text else section_text)
+                elif role == 'beslissing':
+                    beslissing_text += (' ' + section_text if beslissing_text else section_text)
+                else:  # This includes 'overwegingen' or sections without a role
+                    overwegingen_text += (' ' + section_text if overwegingen_text else section_text)
+
+            # Check if critical sections are present
+            if not procesverloop_text or not beslissing_text:
+                return None  # Skip file if critical sections are missing
+
+            # Compile all extracted information into a list
+            judgement_list = [ecli, date, inhoud, legal_body, rechtsgebied, wetsverwijzing, procesverloop_text, overwegingen_text, beslissing_text]
+            return judgement_list
+
+
+    def process_files_in_parallel(self, files):
+        num_processes = multiprocessing.cpu_count()
+        logger.info(f"Multiprocessing with {num_processes}.")
+        pool = multiprocessing.Pool(processes=num_processes)
+
+        # Use the multiprocessing pool to process the XML files in parallel
+        logger.info("start multiprocessing here")
+        result_lists = pool.map(self.process_xml, files)
+        logger.info(f"Number of files processed: {len(result_lists)}")
+
+        # Close the pool and wait for the worker processes to finish
+        pool.close()
+        pool.join()
+
+        return result_lists
+
+    @staticmethod
+    def organize_by_number(strings):
+        result = defaultdict(list)
+        for string in strings:
+            period_index = string.find('.')
+            if period_index != -1:
+                number = string[:period_index]
+                if number.isdigit():
+                    result[int(number)].append(string)
+
+        # Join the lists into a single string per key
+        for key in result:
+            result[key] = ' '.join(result[key])
+
+        # Sort the dictionary by keys to ensure order
+        sorted_result = dict(sorted(result.items()))
+        return sorted_result
+
+    def extract_sections(self, input_path: str) -> pd.DataFrame:
+
+        xml_files = [os.path.join(input_path, file) for file in os.listdir(input_path) if file.endswith('.xml')]
+
+        # Process the data
+        result_lists = self.process_files_in_parallel(xml_files)
+        filtered_results = [result for result in result_lists if result is not None]
+
+        # Create df for metadata
+        column_names = ['ecli', 'date', 'inhoudsindicatie', 'instantie',
+                        'rechtsgebied', 'wetsverwijzing', 'procesverloop',
+                        'overwegingen', 'beslissing']
+        df = pd.DataFrame(filtered_results, columns=column_names)
+
+        documents = df[constants.OVERWEGINGEN_COL].tolist()
+
+        split_patterns = util_data_loader.load_txt_file(constants.SPLIT_PATTERNS_PATH)
+        merge_patterns = util_data_loader.load_txt_file(constants.MERGE_PATTERNS_PATH)
+
+        segmented = []
+        for i, text in enumerate(documents):
+            if i % 100 == 0:
+                logger.info(f"Processing document number: {i} of {len(documents)}")
+            sections = self.text_sectioning(text, split_patterns, merge_patterns)
+            sections = [item for item in sections if item is not None]
+            segmented.append(sections)
+
+        df[constants.SECTIONS_COL] = segmented
+
+        df[constants.SECTIONS_COL] = df[constants.SECTIONS_COL].apply(self.organize_by_number)
+
+        return df
+
